1e7241e74468b8ab613287f593de8a98b86b536b
Yannick <yannick.tollenaere@quorumsoftware.com>
2024-01-27 21:39:10 +0100

    UOM fixed in new ingredients server

--- src/main/java/nl/jemaja/weekmenu/model/UOM.java
+++ src/main/java/nl/jemaja/weekmenu/model/UOM.java
@@ -1,25 +1,45 @@
 package nl.jemaja.weekmenu.model;
 
-public enum UOM {
-	PIECE("piece","pieces"),
-	GRAM("gram","grams"),
-	KG("kilogram","kilograms"),
-	LITRE("liter","liters"),
-	ML("milliliter","milliliter"),
-	SPOON("spoon","spoons"),
-	TABLESPOON("tablespoon","tablespoon");
+import java.sql.Date;
+import java.time.OffsetDateTime;
 
-	UOM(String single, String plural) {
-		this.single = single;
-		this.plural = plural;
-	}
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+public enum UOM {
+	PIECE("piece","PIECE","a"),
+	GRAM("gram","grams","b"),
+	KG("kilogram","kilograms","c"),
+	LITER("liter","liters","d"),
+	ML("milliliter","milliliter","e"),
+	SPOON("spoon","spoons","f"),
+	TABLESPOON("tablespoon","tablespoon","g");
+
+	private String single;
+	private String plural;
+	private String nog;
+	//UOM(String single, String plural) {
+	//	this.single = single;
+	//	this.plural = plural;
+	//}
 	public String getSingle() {
 		return single;
 	}
-	private String single;
-	private String plural;
-	
-	public String toString() {
-		return this.plural;
+	public String getPlural() {
+		return plural;
 	}
+	
+	public String getNog() {
+		return nog;
+	}
+
+	
+	//public String toString() {
+	//	return this.plural + this.nog;
+	//}
 }
